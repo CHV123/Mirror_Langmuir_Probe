2a1dfe2285696ac8d6665a52b3e21d921d1d17cb
Will M <wcmccart@mit.edu>
2018-04-05 10:52:59 -0400

    Save the stuff from the people upstairs

--- plasma-current-response/cores/current_response_v1_0/CurrentResponse.vhd
+++ plasma-current-response/cores/current_response_v1_0/CurrentResponse.vhd
@@ -12,23 +12,44 @@ entity CurrentResponse is
 
   port (
     adc_clk : in std_logic;             -- adc input clock
-    probe_bias : in signed(13 downto 0);
-    current : out signed(13 downto 0)
+    T_electron_in : in signed(13 downto 0); 
+    I_sat : in signed(13 downto 0);
+    V_floating : in signed(13 downto 0);
+    Expo_result : in signed(13 downto 0);
+    Bias_voltage : in signed(13 downto 0);
+    Resistence : in signed(13 downto 0);
+
+    Expo_adress : out signed(13 downto 0);
+    V_LP : out signed(13 downto 0);
+    T_electron_out : out signed(13 downto 0);
+    V_curr : out signed(13 downto 0)
     );
 
 end entity CurrentResponse;
 
 architecture Behavioral of CurrentResponse is
-    signal bias_mask : signed(31 downto 0) := (others => '0');
 begin  -- architecture Behavioral
-  -- Process to count through the voltage levels
- 
-    adf : process(adc_clk)        
+
+	Pass_through : process(adc_clk)
+	begin
+		if (rising_edge(adc_clk)) then
+			-- Passing electron temerature through to the Div block 
+			T_electron_out <= T_electron_in;
+
+			-- Calculate the diffrence in voltages
+			V_LP <= Bias_voltage - V_floating;
+		end if;
+	end process;
+
+	-- Take the input from Div and Expo blocks
+    first_mltp : process(adc_clk)        
+    variable V_curr_mask integer := 0; 
     begin
-    if rising_edge(adc_clk) then 
-        bias_mask <= to_signed(5*to_integer(probe_bias), bias_mask'length);
-        current <= bias_mask(13 downto 0);
-    end if;
+    	if (rising_edge(adc_clk)) then
+    		V_curr_mask := to_integer(Resistence)*to_integer(I_sat)*(1-to_integer(Expo_result));
+    		V_curr <= to_signed(V_curr_mask,42)(13 downto 0);
+    	end if; 
     end process;
-  
+
+
 end architecture Behavioral;
